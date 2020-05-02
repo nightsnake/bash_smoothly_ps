65d641892f7abd04ea9c6ddac061e83b75d6f8fa
Snake <auchakin@gmail.com>
2020-05-03 00:13:06 +0300

    minor fixes

--- files/bash_prompt.sh
+++ files/bash_prompt.sh
@@ -1,3 +1,29 @@
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;36m\]\u\[\033[00m\]@\[\033[01;32m\]$(hostname -f)\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='\u@$(hostname -f):\w\$ '
+fi
+unset color_prompt force_color_prompt
+
 function __git__branch {
 	git branch 2> /dev/null| sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
@@ -9,7 +35,7 @@ else
 
 fi
 
-alias ls='ls -al --color';
+alias ls='ls -alF --color';
 alias ll='ls -la';
 alias rm='rm -i';
 alias mv='mv -i';
@@ -17,4 +43,11 @@ alias mv='mv -i';
 unset use_color safe_term match_lhs
 export HISTCONTROL=ignoreboth
 HISTFILESIZE=2500
+HISTSIZE=1000
+HISTTIMEFORMAT="%Y/%m/%d %T "
 PATH=$PATH:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+# Alias definitions.
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
